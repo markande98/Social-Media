d5f8aa7b2d790e7400152c962ffa3d6071429b3d
markande98 <gauravbpit1998@gmail.com>
2023-03-10 15:01:41 +0530

    organize the pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
 function App() {
   return (
     <div className="App">
